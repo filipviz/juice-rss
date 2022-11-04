2acddf02e678600ea504918f195085b3a32af3cc
filipv <filipeth@protonmail.com>
2022-11-04 00:33:53 -0400

    Handle metadata resolver issues

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
-const server = Deno.listen({ port: 80 });
+const server = Deno.listen({ port: 8080 });
 
 // IPFS Gateway
-const metadataApi = `https://jbx.mypinata.cloud/ipfs`
+const metadataApi = 'https://jbx.mypinata.cloud/ipfs';
+
+// ENS GraphQL Endpoint
+// const ensUrl = 'https://api.thegraph.com/subgraphs/name/ensdomains/ens'
 
 // GraphQL API
-const graphApi = 'https://api.studio.thegraph.com/query/30654/mainnet-dev/0.5.0'
+const graphApi = 'https://api.studio.thegraph.com/query/30654/mainnet-dev/0.5.0';
 
 // GraphQL query
 const query = `{
@@ -31,7 +34,7 @@ async function serveHttp(conn: Deno.Conn) {
     const res = await fetch(graphApi, {
       body: JSON.stringify({ query }),
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' }
+      headers: { 'Content-Type': 'application/json' },
     })
     const answer = await res.json();
 
@@ -47,26 +50,48 @@ async function serveHttp(conn: Deno.Conn) {
     for(const { projectId, metadataUri, createdAt, owner, currentBalance, handle } of answer.data.projects){
       // Fetch IPFS metadata
       const ipfsRes = await fetch(`${metadataApi}/${metadataUri}`);
-      const metadata = await ipfsRes.json();
+      const metadata = ipfsRes.ok ? await ipfsRes.json() : null;
+
+      // Fetch ENS
+//       const ensQuery = `{ 
+//   domains(where: {resolvedAddress: "${owner.toLowerCase()}"}){
+//     name 
+//   }
+// }`;
+//      console.log(ensQuery);
+//      const ensRes = await fetch(ensUrl, {
+//        body: JSON.stringify({ ensQuery }),
+//        method: 'POST',
+//        headers: { 'Content-Type': 'application/json' },
+//      })
+//      console.log(ensRes);
+//      console.log(ensRes.json());
+//      const ensData = await ensRes.json();
+//      console.log(ensData);
 
       // Add item
       body += `<item>
-      <title>${projectId}: ${metadata.name}</title>
+      <title>${projectId}: ${metadata?.name}</title>
       <link>https://juicebox.money/v2/p/${projectId}/</link>
       <description>Current balance: ${currentBalance / 1000000000000000000 } ETH&lt;br&gt;`
-      if(handle){body += `Handle: ${handle}&lt;br&gt;`; }
-      if(metadata.description){ body += `&lt;br&gt;Description: ${metadata.description}&lt;br&gt;`; }
-      if(metadata.payDisclosure){ body += `&lt;br/&gt;Disclosure: ${metadata.payDisclosure}&lt;br/&gt;&lt;br/&gt;`; }
-      if(metadata.twitter){ body += `Twitter: &lt;a href='https://twitter.com/${metadata.twitter}'&gt;@${metadata.twitter}&lt;/a&gt;`; }
-      if(metadata.discord){ body += ` Discord: &lt;a href='${metadata.discord}'&gt;${metadata.discord}&lt;/a&gt;`; }
+      if(handle){ body += `Handle: ${handle}&lt;br&gt;`; }
+      if(!metadata){
+        body += `Could not resolve project metadata.&lt;br&gt;`;
+      } else {
+        if(metadata.description){ body += `&lt;br&gt;Description: ${metadata.description}&lt;br&gt;`; }
+        if(metadata.payDisclosure){ body += `&lt;br/&gt;Disclosure: ${metadata.payDisclosure}&lt;br/&gt;&lt;br/&gt;`; }
+        if(metadata.twitter){ body += `Twitter: &lt;a href='https://twitter.com/${metadata.twitter}'&gt;@${metadata.twitter}&lt;/a&gt;`; }
+        if(metadata.discord){ body += ` Discord: &lt;a href='${metadata.discord}'&gt;${metadata.discord}&lt;/a&gt;`; }
+      }
+      // body += (ensData.data.name ? `<author>${ensData.data.name}</author>` : `<author>${owner}</author>`)
       body += `</description>
       <author>${owner}</author>
       <guid>${projectId}</guid>
       <pubDate>${new Date(createdAt * 1000).toISOString()}</pubDate>
-      </item>`
+      </item>`;
     }
 
-    // Close RSS XML
+    // Close XML body
     body += `</channel></rss>`;
 
     // XML Response
